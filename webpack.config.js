324473b5b3bf72dc737a745cb832dfcaf7f6e89d
AmateurECE <edtwardy@mtu.edu>
2020-08-19 14:13:07 -0500

    Allow building Reach for Node with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 const path = require('path');
+var nodeExternals = require('webpack-node-externals');
 
 const serverConfig = {
     mode: "production",
     entry: './reach/js/main.js',
+    target: "node",
+    externals: nodeExternals(),
     output: {
         path: path.resolve('./reach/build'),
         filename: 'reach.js'
